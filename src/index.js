4eb1367572e69d97d55c8f235342b1b9e5a70b9e
Mitchell Glazier <mitchell.glazier@telenotes.com>
2020-07-17 20:40:57 -0600

    Masking 10 digit USPhone number and adding unit tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-function sayHiTo(name) {
-  return `Hi, ${name}`;
+const digitsOnly = /^\d+$/;
+
+// function to mask digits into US phone format
+function maskUSPhone(phone) {
+  // returning null when getting null, undefined, or an object that is no string as a parameter
+  if (!phone || typeof phone !== "string") {
+    return null;
+  }
+
+  if (digitsOnly.test(phone) === false || phone.length !== 10) {
+    return phone;
+  }
+
+  // returning the masked value
+  const codeArea = phone.substring(0, 3);
+  const prefix = phone.substring(3, 6);
+  const sufix = phone.substring(6, 10);
+  return `(${codeArea}) ${prefix}-${sufix}`;
 }
 
-const message = sayHiTo("Bruno");
-
-console.log(message);
+export default maskUSPhone;
